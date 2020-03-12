0e392d4039f4d171528480dacfb5f9f9e1224a59
Adarsh Nanwani <adarsh.nanwani@gmail.com>
2020-03-12 17:15:27 +0530

    Add todolist logic.

--- src/components/Todo/TodoList.js
+++ src/components/Todo/TodoList.js
@@ -1,8 +1,26 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import TodoItem from './TodoItem';
 
-const TodoList = () => {
-  return <div>Todo List</div>;
+const TodoList = ({ todos }) => {
+  return (
+    <div>
+      <h2>Todo List</h2>
+      <ul>
+        {todos.length > 0 ? (
+          todos.map(todo => <TodoItem key={todo.id} {...todo} />)
+        ) : (
+          <p>Please add a todo.</p>
+        )}
+      </ul>
+    </div>
+  );
 };
 
-export default TodoList;
+const mapStateToProps = state => {
+  return {
+    todos: state.todos
+  };
+};
+
+export default connect(mapStateToProps, null)(TodoList);
