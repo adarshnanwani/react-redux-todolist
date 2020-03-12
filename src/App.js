f7f1c980cb62dfb765ad2572bbdb330d6ca89fbf
Adarsh Nanwani <adarsh.nanwani@gmail.com>
2020-03-12 15:17:46 +0530

    Combine reducers, create store, and hook up the app.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import store from './store/store';
+import TodoHome from './containers/TodoHome';
 import 'normalize.css/normalize.css';
 import './App.css';
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <Provider store={store}>
+      <div className='App'>
+        <TodoHome />
+      </div>
+    </Provider>
+  );
 }
 
 export default App;
