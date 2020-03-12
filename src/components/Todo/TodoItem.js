ce94eba71a5fc998f64ff374b6c2b41cd4004f84
Adarsh Nanwani <adarsh.nanwani@gmail.com>
2020-03-12 17:23:46 +0530

    Add delete todo functionality.

--- src/components/Todo/TodoItem.js
+++ src/components/Todo/TodoItem.js
@@ -1,7 +1,26 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { deleteTodo } from '../../actions/index';
 
-const TodoItem = ({ text }) => {
-  return <div>{text}</div>;
+const TodoItem = props => {
+  const { text, id, deleteTodoHandler } = props;
+  const handleDelete = () => {
+    deleteTodoHandler(id);
+  };
+  return (
+    <div>
+      <label>
+        <input type='checkbox' /> {text}
+      </label>
+      <button onClick={handleDelete}>Delete</button>
+    </div>
+  );
 };
 
-export default TodoItem;
+const mapDispatchToProps = dispatch => {
+  return {
+    deleteTodoHandler: id => dispatch(deleteTodo(id))
+  };
+};
+
+export default connect(null, mapDispatchToProps)(TodoItem);
