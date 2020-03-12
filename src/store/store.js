9442cad55db47968ec2a9714293d820bdbf457f8
Adarsh Nanwani <adarsh.nanwani@gmail.com>
2020-03-12 17:54:06 +0530

    Fix redux dev tools integration.

--- src/store/store.js
+++ src/store/store.js
@@ -2,11 +2,10 @@ import { createStore, compose, applyMiddleware } from 'redux';
 import { logger } from 'redux-logger';
 import rootReducer from '../reducers/rootReducer';
 
-let composeEnhancers = compose;
-
-if (process.env.NODE_ENV !== 'production') {
-  composeEnhancers = window.__REDUX_DEV_TOOLS_EXTENSION_COMPOSE__ || compose;
-}
+const composeEnhancers =
+  process.env.NODE_ENV === 'development'
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    : null || compose;
 
 const store = createStore(
   rootReducer,
