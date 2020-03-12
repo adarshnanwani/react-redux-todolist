d67e90b5162b29e3ea93be36d05e1a318a4e9f02
Adarsh Nanwani <adarsh.nanwani@gmail.com>
2020-03-12 17:37:37 +0530

    Add update todo funtionality.

--- src/components/Todo/EditTodo.js
+++ src/components/Todo/EditTodo.js
@@ -1,7 +1,28 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import useInputState from '../../hooks/useInputState';
+import { updateTodo } from '../../actions/index';
 
-const EditTodo = () => {
-  return <div>EditTodo</div>;
+const EditTodo = ({ text, id, toggleEdit, updateTodoHandler }) => {
+  const [newText, updateNewText] = useInputState(text);
+  const handleSave = () => {
+    updateTodoHandler(id, newText);
+    toggleEdit();
+  };
+  return (
+    <div>
+      <form onSubmit={handleSave}>
+        <input type='text' value={newText} onChange={updateNewText} />
+        <input type='submit' value='Save' />
+      </form>
+    </div>
+  );
 };
 
-export default EditTodo;
+const mapDispatchToProps = dispatch => {
+  return {
+    updateTodoHandler: (id, newText) => dispatch(updateTodo(id, newText))
+  };
+};
+
+export default connect(null, mapDispatchToProps)(EditTodo);
--- src/components/Todo/TodoItem.js
+++ src/components/Todo/TodoItem.js
@@ -1,20 +1,34 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import EditTodo from './EditTodo';
 import { deleteTodo } from '../../actions/index';
+import useToggleState from '../../hooks/useToggleState';
 
 const TodoItem = props => {
   const { text, id, deleteTodoHandler } = props;
+  const [edit, toggleEdit] = useToggleState();
   const handleDelete = () => {
     deleteTodoHandler(id);
   };
-  return (
+  const handleEdit = () => {
+    toggleEdit();
+  };
+
+  const viewTodo = (
     <div>
       <label>
         <input type='checkbox' /> {text}
       </label>
+      <button onClick={handleEdit}>Edit</button>
       <button onClick={handleDelete}>Delete</button>
     </div>
   );
+
+  const editTodo = <EditTodo text={text} id={id} toggleEdit={toggleEdit} />;
+
+  const output = edit ? editTodo : viewTodo;
+
+  return output;
 };
 
 const mapDispatchToProps = dispatch => {
