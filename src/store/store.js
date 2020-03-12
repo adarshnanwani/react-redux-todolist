ec6604c7f06a65199d26aa8778244de465780e8c
Adarsh Nanwani <adarsh.nanwani@gmail.com>
2020-03-12 16:25:30 +0530

    Add redux logger middleware and redux devtools extension support for dev mode.

--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,16 @@
-import { createStore } from 'redux';
+import { createStore, compose, applyMiddleware } from 'redux';
+import { logger } from 'redux-logger';
 import rootReducer from '../reducers/rootReducer';
 
-const store = createStore(rootReducer);
+let composeEnhancers = compose;
+
+if (process.env.NODE_ENV !== 'production') {
+  composeEnhancers = window.__REDUX_DEV_TOOLS_EXTENSION_COMPOSE__ || compose;
+}
+
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(logger))
+);
 
 export default store;
