b1098cf9d3375bec2a097360cd05867166aea725
Adarsh Nanwani <adarsh.nanwani@gmail.com>
2020-03-12 17:11:07 +0530

    Add todo functionality.

--- src/components/Todo/TodoForm.js
+++ src/components/Todo/TodoForm.js
@@ -1,7 +1,31 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { addTodo } from '../../actions/index';
+import useInputState from '../../hooks/useInputState';
 
-const TodoForm = () => {
-  return <div>TodoForm</div>;
+const TodoForm = props => {
+  const [text, updateText, resetText] = useInputState('');
+  const addTodoHandler = e => {
+    e.preventDefault();
+    props.addTodoHandler(text);
+    resetText();
+  };
+  return (
+    <div>
+      <h2>Todo Form</h2>
+      <form onSubmit={addTodoHandler}>
+        <label>Todo:</label>
+        <input type='text' value={text} onChange={updateText} />
+        <input type='submit' value='Add Todo' />
+      </form>
+    </div>
+  );
 };
 
-export default TodoForm;
+const mapDispatchToProps = dispatch => {
+  return {
+    addTodoHandler: text => dispatch(addTodo(text))
+  };
+};
+
+export default connect(null, mapDispatchToProps)(TodoForm);
